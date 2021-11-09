1c76c214f81fffa9d230cc7b27dc9a4ff910fd14
panoramine <camila.c.a.salles@gmail.com>
2021-11-09 13:10:56 -0800

    refactor exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -11,4 +11,4 @@ puts "The total annual revenue of all stores is: #{Store.sum(:annual_revenue)}"
 
 puts "The average annual revenue for all stores is: #{Store.average(:annual_revenue).to_i}"
 
-puts "#{Store.where(annual_revenue: 1000000...2000000).count} are generating $1M or more in annual revenue."
+puts "#{Store.where(annual_revenue: 1000000...2000000).count} stores are generating $1M or more in annual revenue."
