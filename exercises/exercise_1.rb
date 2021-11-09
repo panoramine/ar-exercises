a3622364629071c60192ef2f6a80902cc249039b
panoramine <camila.c.a.salles@gmail.com>
2021-11-09 11:47:23 -0800

    refactor exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -11,4 +11,4 @@ richmond_store = Store.create(name: "Richmond", annual_revenue: 1260000, mens_ap
 
 gastown_store = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
- puts Store.count
+puts Store.count
