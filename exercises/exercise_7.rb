07c1eafa507742c807f0fb8d8b633d909e4555cc
panoramine <camila.c.a.salles@gmail.com>
2021-11-09 13:42:00 -0800

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,19 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Store name: "
+answer_name = gets.chomp
+
+attempt_store = Store.create(name: answer_name)
+
+if(attempt_store.errors[:name])
+  puts "Store #{attempt_store.errors[:name][0]}" 
+end
+
+if(attempt_store.errors[:annual_revenue])
+  puts "Annual revenue #{attempt_store.errors[:annual_revenue][0]}" 
+end
+
+if(attempt_store.errors[:apparel_type])
+  puts "Apparel #{attempt_store.errors[:apparel][0]}" 
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def must_carry_mens_or_womens_apparel
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:apparel, "Store must contain men's or women's apparel.")
+    end
+  end
+
 end
